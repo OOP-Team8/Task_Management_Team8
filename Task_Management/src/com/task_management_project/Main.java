4bf24b75248293f221f9a6857afa02abd5a2a780
IvanPuhniev <vanyo200@gmail.com>
2024-05-13 11:07:53 +0300

    Test Ivan P

--- Task_Management/src/com/task_management_project/Main.java
+++ Task_Management/src/com/task_management_project/Main.java
@@ -5,7 +5,6 @@ package com.task_management_project;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Test");
-
         System.out.println("Ivan Puhniev");
     }
 }
