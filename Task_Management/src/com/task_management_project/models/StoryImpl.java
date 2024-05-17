55af0efe09eb7d9905ec8dffdf6c60fdf2218a44
IvanPuhniev <ivan.puhniev.a60@learn.telerikacademy.com>
2024-05-17 18:25:10 +0300

    Filled in StoryImpl

--- Task_Management/src/com/task_management_project/models/StoryImpl.java
+++ Task_Management/src/com/task_management_project/models/StoryImpl.java
@@ -7,11 +7,39 @@ import com.task_management_project.models.enums.Size;
 import com.task_management_project.models.enums.StoryStatus;
 
 public class StoryImpl extends TaskImpl implements Story {
+
+    private  Priority priority;
+    private  StoryStatus storyStatus;
+    private Size size;
+    private Person person;
+
     public StoryImpl(int id, String title, String description, Priority priority, StoryStatus storyStatus, Size size, Person person) {
         super(id, title, description);
+        this.priority = priority;
+        this.storyStatus = storyStatus;
+        this.size = size;
+        this.person = person;
     }
 
+    @Override
+    public Priority getPriority(){
+        return this.priority;
+    }
 
+    @Override
+    public StoryStatus getStatus() {
+        return this.storyStatus;
+    }
+
+    @Override
+    public Size getSize() {
+        return this.size;
+    }
+
+    @Override
+    public Person getPerson() {
+        return person;
+    }
 
     //TODO
     @Override
--- Task_Management/src/com/task_management_project/models/contracts/Story.java
+++ Task_Management/src/com/task_management_project/models/contracts/Story.java
@@ -1,4 +1,13 @@
 package com.task_management_project.models.contracts;
 
+import com.task_management_project.models.enums.Priority;
+import com.task_management_project.models.enums.Size;
+import com.task_management_project.models.enums.StoryStatus;
+
 public interface Story extends Task,Identifiable{
+
+    Priority getPriority();
+    StoryStatus getStatus();
+    Size getSize();
+    Person getPerson();
 }
